513f9ffd3d9d9e56c4c5ba80aebfc9068d34a07f
Seyma SARIGİL <seymasargil@gmail.com>
2023-01-25 16:31:54 +0300

    __main__ updated for DeprecationWarning: There is no current event loop

--- main.py
+++ main.py
@@ -13,11 +13,13 @@ from app.server import init_queue
 if __name__ == "__main__":
     os.environ["ENV"] = config.ENV  # uygulama çalıştırılırken parametre olarak girilmesi sağlanıp,
     os.environ["DEBUG"] = str(True if config.ENV != "production" else False)
-    loop = asyncio.get_event_loop()
     t1 = threading.Thread(target=uvicorn.run, args=("app.server:app",), kwargs={"host": config.APP_HOST,
                                                                                 "port": config.APP_PORT,
                                                                                 "timeout_keep_alive":
                                                                                     config.MICROSERVICE_CLIENT_TIMEOUT})
+
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
     t2 = threading.Thread(target=loop.run_until_complete, args=(init_queue(),))
     t1.start()
     t2.start()
